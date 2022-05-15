2ec0fdc7a43889019aaaa7474a9797c4be64f085
nomoreFt <hyunwoo.kim1108@gmail.com>
2022-05-15 19:17:08 +0900

    commit

--- src/main/java/hello/thymeleafbasic/basic/TemplateController.java
+++ src/main/java/hello/thymeleafbasic/basic/TemplateController.java
@@ -13,7 +13,7 @@ public class TemplateController {
         return "template/fragment/fragmentMain";
     }
 
-    @GetMapping("layout")
+    @GetMapping("layout")s
     public String layout() {
         return "template/layout/layoutMain";
     }
